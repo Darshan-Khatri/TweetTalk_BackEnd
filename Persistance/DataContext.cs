7cbaedcd0f13ecef1ce351e20ab98fbc1d22ee65
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-12-26 17:49:45 -0600

    Add project files.

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using DatingApplicationBackEnd.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApplicationBackEnd.Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
