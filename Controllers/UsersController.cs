6ca9058034a1c36441dcd083199c2bffacc5920d
Darshan-Khatri <darshankhatri2297@gmail.com>
2020-12-26 18:17:41 -0600

    Section2: Basic Api Creation

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DatingApplicationBackEnd.Core.Models;
+using DatingApplicationBackEnd.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,14 +9,32 @@ using System.Threading.Tasks;
 
 namespace DatingApplicationBackEnd.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Index()
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
         {
-            return Ok("Working");
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUser()
+        {
+            var query = await context.Users.ToListAsync();
+            return Ok(query);
+        }
+
+        //If you don't specify anything as input parameter in HttpGet request and use paramter in action method then 
+        //that parameter becomes the optional parameter/parameters.
+        //Bu t for now we are making it compulsary so we are explicitily defining parameter in curly brackets{Id}.
+        [HttpGet("GetUser/{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
+        {
+            var query = await context.Users.FindAsync(Id);
+            return Ok(query);
         }
     }
 }
